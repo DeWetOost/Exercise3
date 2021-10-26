135b47284a78f770b30a3fe09f1d989ee2c2f211
De Wet Oosthuizen <dewetharm@gmail.com>
2021-10-26 19:50:23 +0100

    Added open account

--- TheBanker/Program.cs
+++ TheBanker/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TheBanker
 {
+    // class representing a bank account with these properties
+    public class BankAccount
+    {
+        public string Name { get; }
+        public double Balance { get; }
+        public string Number { get; set; }
+
+        public BankAccount(string accountName, double initialBalance, string accountNumber)
+        {
+            this.Name = accountName;
+            this.Balance = initialBalance;
+            this.Number = accountNumber;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //main loop
+            List<BankAccount> bank = new List<BankAccount>();
+            bool incorrect = true;
+            while (incorrect)
+            {
+                Console.WriteLine("1. Open account");
+                Console.WriteLine("2. List accounts");
+                Console.WriteLine("3. Show (individual) account details");
+                Console.WriteLine("4. Enter deposit");
+                Console.WriteLine("5. Enter withdrawal");
+                Console.WriteLine("6. Enter transfer");
+                Console.WriteLine("X. Exit");
+
+                // give user the option to choose which menu item to action
+                Console.WriteLine("Please select which option you would like to view and press 'Enter': ");
+                string userSelect = Console.ReadLine();
+
+                // open a new account
+                if (userSelect == "1")
+                {
+                    // ask user for account name and initial balance
+                    Console.WriteLine("Enter account name: ");
+                    string accountName = Console.ReadLine();
+                    Console.WriteLine("Enter initial balance: ");
+                    double initialBalance = double.Parse(Console.ReadLine());
+
+                    // assign random account number
+                    string startWith = "11";
+                    Random generator = new Random();
+                    string r = generator.Next(0, 999999).ToString("D6");
+                    string accountNumber = startWith + r;
+
+                    bank.Add(new BankAccount(accountName, initialBalance, accountNumber));
+                    Console.WriteLine("Bank account added!");
+                    Console.ReadLine();
+                }
+
+                // list accounts command to enable user to view each account (Number, Name, Balance)
+                else if (userSelect == "2")
+                {
+                    Console.WriteLine("List of accounts:");
+                    Console.WriteLine(bank);
+                }
+                    
+                // close list if user select exit
+                else if (userSelect == "X")
+                {
+                    incorrect = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Syntax!");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
